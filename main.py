6d0e613c0e1d11c6236fd6323a46d5b48979326a
niranjantechofficial <61946705+niranjantechofficial@users.noreply.github.com>
2023-02-04 10:31:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ async def account_login(bot: Client, m: Message):
                 Show = f"**Downloading Videos**\n"
                 prog = await m.reply_text(Show)
                 try:
-                  helper.pull_run(thread,cmd)
+                 # helper.pull_run(thread,cmd)
                   for i in range(len(cmd)):
                     try:
                         name = (cmd[i][6])
