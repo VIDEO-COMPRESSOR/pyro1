64eeef7ff604477ae5285d876a56d3af9fe555f9
niranjantechofficial <61946705+niranjantechofficial@users.noreply.github.com>
2023-02-07 20:35:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,8 +162,8 @@ async def login(bot: Client, m: Message):
     raw_text2 = "0" #resolution
     thumb = "no"
     await m.reply_text("Succesfully Bot Loaded 😍🥰.")
-    t1 = threading.Thread(target=account_login, args=(bot, m)) 
-    t2 = threading.Thread(target=account_login, args=(bot, m)) 
+    t1 = threading.Thread(target=await account_login, args=(bot, m)) 
+    t2 = threading.Thread(target=await account_login, args=(bot, m)) 
     t1.start()
     t2.start()
     t1.join()
